3e3eaa950c0b7e75360ffedb894857aeead25ecc
Hanna Oh Andrews <hannaohandrews@MacBook-Pro.local>
2020-08-07 10:09:10 -0700

    hooks!

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(next) {
+    setMode(next);
+    setHistory([...history, next]);
+  }
+
+  function back() {
+    if (history.length === 1) {
+      setMode(initial);
+    } else {
+      setMode(history[history.length - 2]);
+      setHistory(history.slice(0, -1));
+    }
+  }
+
+  return { mode, transition, back };
+}
