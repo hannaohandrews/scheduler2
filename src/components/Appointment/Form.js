f31e0b22d9fc915e5a11cef78ccee6605ba862f6
Hanna Oh Andrews <hannaohandrews@MacBook-Pro.local>
2020-08-10 20:03:44 -0700

    error message testing

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,8 @@ import InterviewerList from "../InterviewerList";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
   function reset() {
     setName("");
     setInterviewer(null);
@@ -14,7 +16,16 @@ export default function Form(props) {
     reset();
     props.onCancel();
   };
-  
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -30,6 +41,7 @@ export default function Form(props) {
             }}
             data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={props.interviewers}
